887d8cf6a20b30556a528f244ea81e0960dab0ab
Ashish-Saha <ashishsahalws@gmail.comgit congig --listgit config --global user.email ashishsahalws@gmail.com>
2026-07-14 08:38:22 +0600

    working with Task modal

--- src/task/Taskboard.jsx
+++ src/task/Taskboard.jsx
@@ -2,32 +2,32 @@ import { useState } from "react";
 import Search from "./Search";
 import TaskActions from "./TaskActions";
 import TaskLists from "./TaskLists";
+import TaskModal from "./TaskModal"
 
 export default function Taskboard() {
-    const initialTask = {
-        id : crypto.randomUUID(),
-        title : "Integration API",
-        description : 'Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.',
-        tags : ["Web", "Python", "API"],
-        priority : 'High',
-        isFavorite : true
-
-    }
-    const [tasks, setTasks] = useState([initialTask])
+  const initialTask = {
+    id: crypto.randomUUID(),
+    title: "Integration API",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tags: ["Web", "Python", "API"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([initialTask]);
 
   return (
     <>
       <section className="mb-20" id="tasks">
+        <TaskModal/>
         <div className="container">
           <div className="p-2 flex justify-end">
-            {/* Search Component */}
             <Search />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            {/* TaskActions Component */}
             <TaskActions />
-            <TaskLists tasks={tasks}/>
+            <TaskLists tasks={tasks} />
           </div>
         </div>
       </section>
