2bc04141e163f241b8db713e49b416b760ea8a7e
Ashish-Saha <ashishsahalws@gmail.com>
2026-07-14 18:15:39 +0600

    Completing task modal controll component

--- src/task/TaskModal.jsx
+++ src/task/TaskModal.jsx
@@ -1,53 +1,78 @@
-export default function TaskModal() {
+import { useState } from "react";
+
+export default function TaskModal({ onCloseModal }) {
+  const defaultForm = {
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFavorite: false,
+  };
+
+  const [formData, setFormData] = useState(defaultForm);
+  console.log(formData);
+  const handleForm = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "tags") {
+      value = value.split(",");
+    }
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
     <>
-      <div className="bg-black bg-opacity-70 h-full w-full z-10 absolute top-0 left-0 "></div>
-      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-1/4 left-1/4 ">
+      <div className="fixed inset-0 bg-black/75 backdrop-blur-sm z-40"></div>
+      <form className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[500px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:p-11">
         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
           Add New Task
         </h2>
 
         <div className="space-y-9 text-white lg:space-y-10">
           <div className="space-y-2 lg:space-y-3">
-            <label for="title">Title</label>
+            <label htmlFor="title">Title</label>
             <input
               className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
               type="text"
               name="title"
-              id="title"
+              value={formData.title}
+              onChange={handleForm}
               required
             />
           </div>
 
           <div className="space-y-2 lg:space-y-3">
-            <label for="description">Description</label>
+            <label htmlFor="description">Description</label>
             <textarea
-              className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+              className="block min-h-[50px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[50px]"
               type="text"
               name="description"
-              id="description"
+              value={formData.description}
+              onChange={handleForm}
               required
             ></textarea>
           </div>
 
           <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
             <div className="space-y-2 lg:space-y-3">
-              <label for="tags">Tags</label>
+              <label htmlFor="tags">Tags</label>
               <input
                 className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
                 type="text"
                 name="tags"
-                id="tags"
+                value={formData.tags}
+                onChange={handleForm}
                 required
               />
             </div>
 
             <div className="space-y-2 lg:space-y-3">
-              <label for="priority">Priority</label>
+              <label htmlFor="priority">Priority</label>
               <select
                 className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
                 name="priority"
-                id="priority"
+                value={formData.title}
+                onChange={handleForm}
                 required
               >
                 <option value="">Select Priority</option>
@@ -59,12 +84,20 @@ export default function TaskModal() {
           </div>
         </div>
 
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between  lg:mt-20">
           <button
             type="submit"
             className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
           >
             Create new Task
+          </button>
+
+          <button
+            type="button"
+            className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={onCloseModal}
+          >
+            Close
           </button>
         </div>
       </form>
--- src/task/Taskboard.jsx
+++ src/task/Taskboard.jsx
@@ -15,18 +15,21 @@ export default function Taskboard() {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([initialTask]);
+  const [showModal, setShowModal] = useState(false)
+
+  
 
   return (
     <>
       <section className="mb-20" id="tasks">
-        <TaskModal/>
+        {showModal && <TaskModal onCloseModal={()=>setShowModal(false)}/>}
         <div className="container">
           <div className="p-2 flex justify-end">
             <Search />
           </div>
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskActions />
+            <TaskActions onAddClick={()=> setShowModal(true)}/>
             <TaskLists tasks={tasks} />
           </div>
         </div>
